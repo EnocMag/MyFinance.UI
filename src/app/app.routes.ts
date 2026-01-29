ea90fe2746c2ac13dfe6df1f2c1328d1b74594d0
Enoc <magallon.estrada.e@gmail.com>
2026-01-28 21:25:41 -0600

    feat: restructure routing configuration for improved readability and organization

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,15 @@ import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
-    { path: 'login', loadComponent: () => import('./components/login-form/login-form').then(m => m.LoginForm) },
-    { path: 'transactions', loadComponent: () => import('./components/transaction-form/transaction-form').then(m => m.TransactionForm),
-        canActivate: [authGuard]
+    { path: 'login', 
+        loadComponent: () => import('./components/login-form/login-form').then(m => m.LoginForm) },
+    { path: '',
+        loadComponent: () => import('./components/layout/layout').then(m => m.Layout),
+        canActivate: [authGuard],
+        children: [
+            { path: 'transactions', loadComponent: () => import('./components/transaction-form/transaction-form').then(m => m.TransactionForm) },
+            { path: 'categories', loadComponent: () => import('./components/category-form/category-form').then(m => m.CategoryForm) }
+        ]
     },
+    { path: '**', redirectTo: 'transactions' },
 ];
