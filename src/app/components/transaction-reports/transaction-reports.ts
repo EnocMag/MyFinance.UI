591c0979f6b0dd51edbd733c92af70eccb538b2f
Enoc <magallon.estrada.e@gmail.com>
2026-02-02 23:42:35 -0600

    feat: implement OnInit lifecycle and refactor transaction retrieval to use Observables

--- src/app/components/transaction-reports/transaction-reports.ts
+++ src/app/components/transaction-reports/transaction-reports.ts
@@ -1,21 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TransactionService } from '../../services/transaction-service';
 import { Transaction } from "../../models/transaction-model";
-
+import { CommonModule } from '@angular/common';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-transaction-reports',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './transaction-reports.html',
   styleUrl: './transaction-reports.css',
 })
-export class TransactionReports {
+export class TransactionReports implements OnInit {
   constructor(private transactionService: TransactionService) {}
-  public transactions: Transaction[] = [];
+  public transactions$ : Observable<Transaction[]> | undefined = undefined;
 
-  ngOnInit() {
-    this.transactionService.getAllTransactions().subscribe((response) => {
-      this.transactions = response.data;
-    })
+  ngOnInit(): void {
+    this.transactions$ = this.transactionService.getAllTransactions().pipe(map(res =>{
+      return res.data
+    }));
   }
 }
