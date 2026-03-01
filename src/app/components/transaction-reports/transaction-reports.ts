6f8aa33fdfedec16f3a2997493c981cc0ec3cec8
Enoc <magallon.estrada.e@gmail.com>
2026-02-28 22:43:38 -0600

    feat: add action buttons for editing and deleting transactions in reports

--- src/app/components/transaction-reports/transaction-reports.ts
+++ src/app/components/transaction-reports/transaction-reports.ts
@@ -3,10 +3,11 @@ import { TransactionService } from '../../services/transaction-service';
 import { Transaction } from "../../models/transaction-model";
 import { CommonModule } from '@angular/common';
 import { map, Observable } from 'rxjs';
+import { ConfirmationDialog } from '../confirmation-dialog/confirmation-dialog';
 
 @Component({
   selector: 'app-transaction-reports',
-  imports: [CommonModule],
+  imports: [CommonModule, ConfirmationDialog],
   templateUrl: './transaction-reports.html',
   styleUrl: './transaction-reports.css',
 })
@@ -18,5 +19,28 @@ export class TransactionReports implements OnInit {
     this.transactions$ = this.transactionService.getAllTransactions().pipe(map(res =>{
       return res.data
     }));
+  }
+  isDialogOpen: boolean = false;
+
+  editTransaction(transaction: Transaction): void {
+    // TODO: Implementar lógica para editar transacción
+    console.log('Edit transaction:', transaction);
+  }
+
+  deleteTransaction(transactionId: number): void {
+    // TODO: Implementar lógica para eliminar transacción
+    this.isDialogOpen = true;
+    console.log('Delete transaction with ID:', transactionId);
+  }
+
+  handleConfirm(): void {
+    this.isDialogOpen = false;
+    console.log('Transaction deleted');
+    // Aquí puedes agregar la lógica para eliminar la transacción
+  }
+  handleCancel(): void {
+    this.isDialogOpen = false;
+    console.log('Deletion cancelled');
+    // Aquí puedes agregar la lógica para cancelar la eliminación
   }
 }
