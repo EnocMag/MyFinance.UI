59899d6b331211f0199cee9a212d50e14ef17278
Enoc <magallon.estrada.e@gmail.com>
2026-03-11 14:25:48 -0600

    feat: add category filtering to transaction reports and refactor date filtering logic

--- src/app/components/transaction-reports/transaction-reports.ts
+++ src/app/components/transaction-reports/transaction-reports.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { TransactionService } from '../../services/transaction-service';
+import { CategoryService } from '../../services/category-service';
 import { Transaction } from "../../models/transaction-model";
+import { Category } from "../../models/category-model";
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { map, Observable } from 'rxjs';
@@ -13,16 +15,29 @@ import { ConfirmationDialog } from '../confirmation-dialog/confirmation-dialog';
   styleUrl: './transaction-reports.css',
 })
 export class TransactionReports implements OnInit {
-  constructor(private transactionService: TransactionService) {}
+  constructor(
+    private transactionService: TransactionService,
+    private categoryService: CategoryService
+  ) {}
   
   public transactions$ : Observable<Transaction[]> | undefined = undefined;
+  public categories$: Observable<Category[]> | undefined = undefined;
   public startDate: string = '';
   public endDate: string = '';
+  public selectedCategory: string = '';
   
   private allTransactions: Transaction[] = [];
+  private allCategories: Category[] = [];
   private transactionIdToDelete: number | null = null;
 
   ngOnInit(): void {
+    // Cargar categorías
+    this.categories$ = this.categoryService.getAllCategories().pipe(map(res => {
+      this.allCategories = res.data;
+      return res.data;
+    }));
+    
+    // Cargar transacciones
     this.transactions$ = this.transactionService.getAllTransactions().pipe(map(res => {
       this.allTransactions = res.data;
       return res.data;
@@ -30,18 +45,29 @@ export class TransactionReports implements OnInit {
   }
 
   filterByDate(): void {
-    if (!this.startDate && !this.endDate) {
-      this.transactions$ = this.transactionService.getAllTransactions().pipe(map(res => res.data));
-      return;
+    this.applyFilters();
+  }
+
+  public applyFilters(): void {
+    let filtered = [...this.allTransactions];
+
+    // Filtro por fecha
+    if (this.startDate || this.endDate) {
+      const start = this.startDate ? new Date(this.startDate) : new Date('1900-01-01');
+      const end = this.endDate ? new Date(this.endDate) : new Date('2100-12-31');
+
+      filtered = filtered.filter(transaction => {
+        const transactionDate = new Date(transaction.date);
+        return transactionDate >= start && transactionDate <= end;
+      });
     }
 
-    const start = this.startDate ? new Date(this.startDate) : new Date('1900-01-01');
-    const end = this.endDate ? new Date(this.endDate) : new Date('2100-12-31');
-
-    const filtered = this.allTransactions.filter(transaction => {
-      const transactionDate = new Date(transaction.date);
-      return transactionDate >= start && transactionDate <= end;
-    });
+    // Filtro por categoría
+    if (this.selectedCategory) {
+      filtered = filtered.filter(transaction => 
+        transaction.categoryNameSnapshot === this.selectedCategory
+      );
+    }
 
     this.transactions$ = new Observable(observer => {
       observer.next(filtered);
@@ -52,6 +78,7 @@ export class TransactionReports implements OnInit {
   clearFilters(): void {
     this.startDate = '';
     this.endDate = '';
+    this.selectedCategory = '';
     this.transactions$ = this.transactionService.getAllTransactions().pipe(map(res => res.data));
   }
 
