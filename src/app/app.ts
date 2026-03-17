dcabe225bdc39e9bb5f6b53198c129e2de37d617
Enoc <magallon.estrada.e@gmail.com>
2026-03-17 15:01:15 -0600

    feat: remove login component and associated authentication services

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,5 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TransactionForm } from './components/transaction-form/transaction-form';
-import { LoginForm } from "./components/login-form/login-form";
 
 @Component({
   selector: 'app-root',
